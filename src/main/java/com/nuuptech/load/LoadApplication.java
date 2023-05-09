1ec9510978367358b790fccdf11f33b11ac29df2
Jorge Medina <tazvoit@gmail.com>
2023-05-09 08:46:38 -0600

    Update LoadApplication.java

--- src/main/java/com/nuuptech/load/LoadApplication.java
+++ src/main/java/com/nuuptech/load/LoadApplication.java
@@ -2,8 +2,10 @@ package com.nuuptech.load;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:/config/application.properties")
 public class LoadApplication {
 
 	public static void main(String[] args) {
