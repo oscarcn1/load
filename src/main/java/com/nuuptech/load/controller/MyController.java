b2881ad1bc9756f780d51a3f9d333770d863f286
Oscar <oscar.cn1@gmail.com>
2023-05-08 19:25:08 -0600

    Endpoints de carga de memoria, cpu y pausa.

---
+++ src/main/java/com/nuuptech/load/controller/MyController.java
@@ -0,0 +1,37 @@
+package com.nuuptech.load.controller;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    private List<byte[]> bigList = new ArrayList<>();
+
+    @GetMapping("/consume-cpu")
+    public String consumeCpu() {
+        for(int i = 0; i < 1000000000; i++) {
+            double j = Math.pow(i, 2);
+        }
+        return "CPU process completed";
+    }
+
+    @GetMapping("/consume-memory")
+    public String consumeMemory() {
+        byte[] bigObject = new byte[1024 * 1024 * 100]; // Crea un objeto grande de 100 MB
+        bigList.add(bigObject); // Agrega el objeto a la lista
+        long memoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        double totalMemoryInMB = (double) memoryUsed / (1024 * 1024);
+        return "100 MB object added to the list. Memory used: " + new DecimalFormat("#.##").format(totalMemoryInMB) + " Mb";
+    }
+
+    @GetMapping("/sleep-miliseconds")
+    public String sleepMilieconds() throws InterruptedException {
+        Thread.sleep(600);
+        return "Responding after sleep";
+    }
+
+}
